9cbe50b16b6216d1dd3fbf29cda87b5cbea5fbc3
lesguillemets <lesguillemets@users.noreply.github.com>
2016-11-10 07:23:35 +0900

    wip : better interface

--- src/Plot.hs
+++ src/Plot.hs
@@ -1,22 +1,67 @@
-module Plot ( Plot
-            , plot
-            , draw
+{-# LANGUAGE RecordWildCards #-}
+module Plot( PlotImg
+           , Plot (..)
+           , Range (..)
+           , plot
+           , plt
+           , draw
     ) where
 
 import Prelude hiding (putStr, unlines)
 import Data.List hiding (unlines)
 import Data.Monoid
+import Data.Maybe (fromMaybe)
 import Data.ByteString.Lazy (ByteString, putStr)
 import Data.ByteString.Lazy.Builder as BLB
 
-type Range = (Double, Double)
-newtype Plot = Plot ByteString
+newtype PlotImg = PlotImg ByteString
+
+data  Range = Range (Double, Double)
+            | Unspecified
+instance Show Range where
+    show (Range (t0,t1)) = "from " <> show t0 <> " to " <> show t1
+    show Unspecified = "unspecified"
+
+unspecified :: Range -> Bool
+unspecified Unspecified = True
+unspecified _ = False
+
+getRangeDefault :: (Double,Double) -> Range -> (Double, Double)
+getRangeDefault def Unspecified = def
+getRangeDefault _ (Range r) = r
+
+data Plot = Plot { func :: Double -> Double
+                 , name :: Maybe String
+                 , xRange :: Range
+                 , yRange :: Range
+                 , xN :: Int
+                 , yN :: Int }
+instance Show Plot where
+    show (Plot {..})
+        = (fromMaybe "" $ (<> ":") <$> name)
+        <>  " x " <> show xRange <> " y " <> show yRange
+
+plt :: Plot -> PlotImg
+plt p@(Plot {..}) =
+    let (x0, x1) = getRangeDefault ((-1), 1) xRange
+        dx = (x1-x0) / (fromIntegral xN)
+        values = map func . take xN $ [x0, x0+dx..]
+        yMax = maximum values
+        yMin = minimum values
+        dy = if yMax == yMin
+                then 1
+                else (yMax - yMin) / fromIntegral yN
+        cutY y0 = mconcatMap (toDot (y0-dy, y0)) values
+        in
+    PlotImg . toLazyByteString . mconcatMap ((<> char7 '\n') . cutY)
+        . take (yN+2) $ [yMax+dy, yMax..]
+
 plot :: Double
      -> Double
      -> Int
      -> Int
      -> (Double -> Double)
-     -> Plot
+     -> PlotImg
 plot x0 x1 n h f =
     let dx = (x1-x0) / (fromIntegral n)
         values = map f . take n $ [x0, x0+dx..]
@@ -27,16 +72,16 @@ plot x0 x1 n h f =
                 else (yMax - yMin) / fromIntegral h
         cutY y0 = mconcatMap (toDot (y0-dy, y0)) values
         in
-    Plot . toLazyByteString . mconcatMap ((<> char7 '\n') . cutY)
+    PlotImg . toLazyByteString . mconcatMap ((<> char7 '\n') . cutY)
         . take (h+2) $ [yMax+dy, yMax..]
 
 mconcatMap :: Monoid c => (a -> c) -> [a] -> c
 mconcatMap f = mconcat . map f
 
-draw :: Plot -> IO ()
-draw (Plot p) = putStr p
+draw :: PlotImg -> IO ()
+draw (PlotImg p) = putStr p
 
-toDot :: Range -> Double -> Builder
+toDot :: (Double,Double) -> Double -> Builder
 toDot (l,h) x
     | x < l =           char7 ' '
     | x <= middleLow  = char7 '.'
