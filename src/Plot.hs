b3fb882c963c06f54db8c0e54dcb3f3faf1c9c04
lesguillemets <lesguillemets@users.noreply.github.com>
2016-11-09 15:34:22 +0900

    use builder

--- src/Plot.hs
+++ src/Plot.hs
@@ -1,13 +1,18 @@
 module Plot where
-import Data.List
+import Prelude hiding (putStr, unlines)
+import Data.List hiding (unlines)
+import Data.Monoid
+import Data.ByteString.Lazy (ByteString, putStr)
+import Data.ByteString.Lazy.Builder as BLB
 
 type Range = (Double, Double)
+newtype Plot = Plot ByteString
 plot :: Double
      -> Double
      -> Int
      -> Int
      -> (Double -> Double)
-     -> [String]
+     -> Plot
 plot x0 x1 n h f =
     let dx = (x1-x0) / (fromIntegral n)
         values = map f . take n $ [x0, x0+dx..]
@@ -16,18 +21,25 @@ plot x0 x1 n h f =
         dy = if yMax == yMin
                 then 1
                 else (yMax - yMin) / fromIntegral h
-        cutY y0 = map (toDot (y0-dy, y0)) values
+        cutY y0 = mconcatMap (toDot (y0-dy, y0)) values
         in
-    map cutY . take (h+2) $ [yMax+dy, yMax..]
+    Plot . toLazyByteString . mconcatMap ((<> char7 '\n') . cutY)
+        . take (h+2) $ [yMax+dy, yMax..]
 
-toDot :: Range -> Double -> Char
+mconcatMap :: Monoid c => (a -> c) -> [a] -> c
+mconcatMap f = mconcat . map f
+
+draw :: Plot -> IO ()
+draw (Plot p) = putStr p
+
+toDot :: Range -> Double -> Builder
 toDot (l,h) x
-    | x < l =           ' '
-    | x <= middleLow  = '.'
-    | x <= middleHigh = '*'
-    | x <= h =          '^'
-    | h < x =           ' '
-    | otherwise =       '!'
+    | x < l =           char7 ' '
+    | x <= middleLow  = char7 '.'
+    | x <= middleHigh = char7 '*'
+    | x <= h =          char7 '^'
+    | h < x =           char7 ' '
+    | otherwise =       char7 '!'
     where
         third = (h-l) / 3
         middleLow = l + third
